5bba8591a402827e4e431e40edb479fcdd141e20
Darío Reyes <eran6reyes@gmail.com>
2020-07-05 19:11:17 -0300

    Implements ops over ref to point

--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,7 @@ impl Point {
         }
     }
 
+    // Accessors
     pub fn angle(&self) -> f64 {
         return (self.y.atan2(self.x) + 2.0*consts::PI) % (2.0*consts::PI);
     }
@@ -30,7 +31,7 @@ impl Point {
         return (self.x.powf(2.0) + self.y.powf(2.0)).sqrt();
     }
     pub fn unit(&self) -> Point {
-        return *self / self.norm();
+        return self / self.norm();
     }
     pub fn rotated(&self, angle : f64) -> Point {
         let mut rotated = *self;
@@ -52,34 +53,32 @@ impl Point {
     }
 }
 
-// TODO: tal vez es una buena idea implementar estos traits para &Point,
-// para que no consuman la instancia.
-// https://doc.rust-lang.org/std/ops/index.html
-impl ops::Add<Point> for Point {
+// https://stackoverflow.com/questions/28005134/how-do-i-implement-the-add-trait-for-a-reference-to-a-struct
+impl ops::Add<&Point> for &Point {
     type Output = Point;
-    fn add(self, other: Point) -> Point {
+    fn add(self, other: &Point) -> Point {
         return Point::new(self.x + other.x, self.y + other.y);
     }
 }
-impl ops::Sub<Point> for Point {
+impl ops::Sub<&Point> for &Point {
     type Output = Point;
-    fn sub(self, other: Point) -> Point {
+    fn sub(self, other: &Point) -> Point {
         return Point::new(self.x - other.x, self.y - other.y);
     }
 }
-impl ops::Mul<f64> for Point {
+impl ops::Mul<f64> for &Point {
     type Output = Point;
     fn mul(self, scalar: f64) -> Point {
         return Point::new(self.x*scalar, self.y*scalar);
     }
 }
-impl ops::Mul<Point> for Point {
+impl ops::Mul<&Point> for &Point {
     type Output = f64;
-    fn mul(self, other: Point) -> f64 {
+    fn mul(self, other: &Point) -> f64 {
         return self.x * other.x + self.y * other.y;
     }
 }
-impl ops::Div<f64> for Point {
+impl ops::Div<f64> for &Point {
     type Output = Point;
     fn div(self, scalar: f64) -> Point {
         return Point::new(self.x/scalar, self.y/scalar);
@@ -137,7 +136,7 @@ mod point_tests {
     fn add() {
         let point = Point::new(1.0, -1.0);
         let other = Point::new(12.0, 1.5);
-        let sum = point + other;
+        let sum = &point + &other;
         assert_in_delta(sum.x, 13.0);
         assert_in_delta(sum.y, 0.5);
     }
@@ -146,7 +145,7 @@ mod point_tests {
     fn sub() {
         let point = Point::new(1.0, -1.0);
         let other = Point::new(12.0, 1.5);
-        let sub = point - other;
+        let sub = &point - &other;
         assert_in_delta(sub.x, -11.0);
         assert_in_delta(sub.y, -2.5);
     }
@@ -154,7 +153,7 @@ mod point_tests {
     #[test]
     fn mul_scalar() {
         let point = Point::new(4.0, -1.0);
-        let point = point * 2.0;
+        let point = &point * 2.0;
         assert_in_delta(point.x, 8.0);
         assert_in_delta(point.y, -2.0);
     }
@@ -163,14 +162,14 @@ mod point_tests {
     fn dot_product() {
         let point = Point::new(2.0, 2.0);
         let other = Point::new(1.0, -3.0);
-        let dot_prod = point * other;
+        let dot_prod = &point * &other;
         assert_in_delta(dot_prod, -4.0);
     }
 
     #[test]
     fn div() {
         let point = Point::new(4.0, -1.0);
-        let point = point / 2.0;
+        let point = &point / 2.0;
         assert_in_delta(point.x, 2.0);
         assert_in_delta(point.y, -0.5);
     }
